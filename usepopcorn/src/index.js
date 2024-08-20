9d93fc322a8989bdb7e0c77fb9ca9bb8d644e7c3
dmarcelin <marcelinthierry@gmail.com>
2024-08-20 06:49:14 -0400

    Add initial app to fecth movies from OMDB API

---
+++ usepopcorn/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+// import StarRating from "./StarRating";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+    {/*<StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating maxRating={10} />
+    <StarRating maxRating={8} color="pink" size={40} /> */}
+  </React.StrictMode>
+);
